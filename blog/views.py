25cead7af10058846c677e27a82674d76f0bfb95
Leemyeongeun <mta23567@naver.com>
2019-10-15 13:13:27 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from .models import Posts
 
 def post_list(request):
     # views.post_list 함수는 이제 DB에서 필요한 데이터를 가져와서
     # post_list.html에게 넘겨줘야 함
-    posts = Post.objects.filter(published_date__lte=timezone.now()).\
+    posts = Posts.objects.filter(published_date__lte=timezone.now()).\
         order_by('-published_date')
         # 출판일자가 지금보다 이전으로 들어있는 행만 검색
     return render(                       # render() 함수를 호출하여 화면을 출력
